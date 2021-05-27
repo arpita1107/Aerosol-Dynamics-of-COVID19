e7da0f5c37598163ad0ef6edf3758707bcc7f776
Arpita <76917638+arpita1107@users.noreply.github.com>
2021-05-27 21:34:53 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,7 @@
+g = 9.81
+d_water = 1000
+T = 25
+p_air = 1.184  # (25 degree C)
+kv = 2  # room ventilation rate(AC/h)
+kd = 5.1986  # particle deposition rate
+pv = 0.36  # pulmonary ventilation rate (6l/min) given pv in m3/h
